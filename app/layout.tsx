51304806de84ce1e1d089932a5329237980028c0
Eranga727 <hansanaeranga2001@gmail.com>
2025-05-02 23:33:00 +0530

    fix: update event description for clarity and detail

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'LETHALBOTS 2025',
-  description: 'LETHALBOTS 2025 is a SLTC Robotic batlle competion.',
+  description: 'Lethal bot 2025, the robot battle organized by Sri Lanka Technological Campus (SLTC), bringing together the brightest innovators from universities across Sri Lanka. As the premier SLTC robot battle event, Lethal bots challenges teams to design, build, and battle their robots in an epic competition of skill, creativity, and teamwork. With thrilling head-to-head battles and groundbreaking robot designs, Lethal bots SLTC is a celebration of technology, engineering excellence, and the future of robotics. Join us at SLTC and experience the excitement of the Lethal bot robot battle.',
   icons: {
     icon: [
       {
